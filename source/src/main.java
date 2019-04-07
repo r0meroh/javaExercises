ec0c015d80c3f3b3fdbd242eb1c401f243785022
r0meroh <romeroh862@gmail.com>
2019-04-07 00:05:30 -0700

    beginnings of java weekly exercises

---
+++ source/src/main.java
@@ -0,0 +1,13 @@
+//this program is my attempt to exercise my java muscle
+//this specific program will eventually have sorting algorithms
+
+
+public class main {
+    public static void main(String[] args){
+        System.out.println("hello");
+        String hi = "hi";
+        System.out.println(hi);
+
+
+    }
+}
