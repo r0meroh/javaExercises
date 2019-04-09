9cc6c57efc165e44142f7a569f79be8ecc6363fd
r0meroh <romeroh862@gmail.com>
2019-04-08 23:32:05 -0700

    testing commit and push. This file was manually uploaded, checking to see if changes on local host will reflect onto online repository

---
+++ source/src/bubbleSort.java
@@ -0,0 +1,22 @@
+public class bubbleSort {
+
+    int[] bubbly (int[] x){
+
+        for(int i = 0; i < x.length - 1; i++){
+            for(int j = 0; j < x.length - i - 1; j++){
+                if(x[j] > x [j+1]){
+                    int temp = x[j];
+                    x[j] = x[j+1];
+                    x[j+1] = temp;
+                }
+            }
+        }
+        return x;
+    }
+    void printBubbles(int[] x){
+        for(int num:x)
+            System.out.print(num + " ");
+    }
+
+}
+
