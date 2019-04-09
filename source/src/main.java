e842b3ad7e1574e657c74b65fa754c63d1458c43
r0meroh <romeroh862@gmail.com>
2019-04-08 23:33:29 -0700

    added bubble sort object to main along with output for original pre-sorted arrays

--- source/src/main.java
+++ source/src/main.java
@@ -4,20 +4,42 @@
 
 public class main {
     public static void main(String[] args) {
+
         //create sort object
         selectionSort sortThis = new selectionSort();
         //initialize array to sort
         int myArray[] = {554,234,77,0,65,234523};
 
+        System.out.println("original: ");
+        sortThis.printSort(myArray);
+        System.out.println(" ");
+
         //test sort
-        System.out.println(sortThis.selectionSort(myArray));
+        //System.out.println(sortThis.selectionSort(myArray));
+        System.out.println("Selection sort");
         //reassign sorted array to original array
         myArray = sortThis.selectionSort(myArray);
         //print newly sorted array
         sortThis.printSort(myArray);
 
 
+        //spacing
+        System.out.println(" ");
+
+
+
         //room for next sort
+        //bubble sort
+        bubbleSort bubbles = new bubbleSort();
+
+        int mySecondArray[] = {11,234,55555,8756,1,3,77,888};
+        System.out.println("Original:  ");
+        bubbles.printBubbles(mySecondArray);
+        System.out.println(" ");
+
+        System.out.println("Bubble Sort ");
+        mySecondArray = bubbles.bubbly(mySecondArray);
+        bubbles.printBubbles(mySecondArray);
     }
 }
 
